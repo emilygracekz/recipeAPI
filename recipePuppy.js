86bb102c88a5669ca4a002c880f7018f82c658af
Emily <57361805+emilygracekz@users.noreply.github.com>
2020-02-23 13:36:47 +0200

    Add files via upload

---
+++ recipePuppy.js
@@ -0,0 +1,44 @@
+const baseEndpoint = 'http://www.recipepuppy.com/api'
+const proxy = "https://cors-anywhere.herokuapp.com/";
+const form = document.querySelector('form.search');
+const recipesGrid = document.querySelector('.recipes');
+
+async function fetchRecipes(query) {
+const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
+const data = await res.json();
+return data;
+}
+
+async function handleSubmit(event) {
+event.preventDefault();
+const el = event.currentTarget;
+fetchAndDisplay(form.query.value);
+}
+
+async function fetchAndDisplay(query){
+//turn the form off
+form.submit.disabled = true;
+
+//submit the search
+const recipes = await fetchRecipes(query);
+console.log(recipes);
+form.submit.disabled = false;
+displayRecipes(recipes.results);
+}
+
+
+function displayRecipes(recipes) {
+    console.log('creating HTML');
+    const html = recipes.map(recipe => `<div class="recipe">
+        <h2>${recipe.title}</h2>
+        <p>${recipe.ingredients}</p>
+        ${recipe.thumbnail && `<img src="${recipe.thumbnail}"
+        alt="${recipe.title}"/>`}
+        <a href="${recipe.href}">View Recipe</a>
+        </div>`
+    );
+    recipesGrid.innerHTML = html.join ('');
+}
+
+form.addEventListener('submit', handleSubmit);
+fetchAndDisplay('pizza');
